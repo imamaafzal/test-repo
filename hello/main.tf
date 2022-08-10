54126c58b1850515174223e4d3edc70132f944d9
imamaafzal <ec2-user@ip-172-31-16-147.ec2.internal>
2022-08-10 00:05:12 +0000

    changes to key

--- hello/main.tf
+++ hello/main.tf
@@ -1,12 +1,7 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIATXMSR7MFHZRA2E3T"
-  secret_key = "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"
-}
 resource "aws_instance" "mynewinstance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = "secondinstance"
-}
+  }
 }
